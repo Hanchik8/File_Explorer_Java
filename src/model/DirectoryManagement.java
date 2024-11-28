aa8c4eba4d66a74368c03a5ec9b4eb716212f65b
Alieva Nargiza <nargiza.alieva018@gmail.com>
2024-11-28 18:40:22 +0600

    add comments to methods

--- src/model/DirectoryManagement.java
+++ src/model/DirectoryManagement.java
@@ -5,18 +5,28 @@ import src.view.View;
 import java.io.File;
 import java.util.Stack;
 
+/**
+ * This class manages directory navigation and undo/redo/moveToParent operations for a file explorer.
+ */
 public class DirectoryManagement {
-    private View explorerView;
-    private String currentDirectory = "Root";
-    private Stack<String> undoStack;
-    private Stack<String> redoStack;
+    private View explorerView; // Reference to the view component for updating view
+    private String currentDirectory = "Root"; // // Tracks the current directory
+    private Stack<String> undoStack; // Stack for undoing directory navigation
+    private Stack<String> redoStack; // Stack for redoing directory navigation
 
+    /**
+     * Constructor initializes the view and stacks for undo/redo functionality.
+     * @param explorerView the view component for updating the view
+     */
     public DirectoryManagement(View explorerView) {
         this.explorerView = explorerView;
         undoStack = new Stack<>();
         redoStack = new Stack<>();
     }
 
+    /**
+     * Navigates to the previous directory in the undo stack.
+     */
     public void undo() {
         if (!undoStack.isEmpty()) {
             redoStack.push(currentDirectory);
@@ -25,6 +35,9 @@ public class DirectoryManagement {
         }
     }
 
+    /**
+     * Navigates to the next directory in the redo stack.
+     */
     public void redo() {
         if (!redoStack.isEmpty()) {
             undoStack.push(currentDirectory);
@@ -33,6 +46,9 @@ public class DirectoryManagement {
         }
     }
 
+    /**
+     * Moves to the parent directory of the current directory.
+     */
     public void moveToParentDirectory() {
         File current = new File(currentDirectory);
         if (current.getParent() != null) {
@@ -43,6 +59,11 @@ public class DirectoryManagement {
         }
     }
 
+    /**
+     * Updates the current directory and retrieves its contents.
+     * @param newDirectory the directory to move to
+     * @return an array of files/directories in the new directory
+     */
     public File[] updateDirectory(String newDirectory) {
         if (isRootDirectory(newDirectory)) {
            return getInitialDirectories();
@@ -55,6 +76,10 @@ public class DirectoryManagement {
         }
     }
 
+    /**
+     * Refreshes the current directory and retrieves its contents.
+     * @return an array of files/directories in the current directory
+     */
     public File[] updateDirectory() {
         if (isRootDirectory(currentDirectory)) {
             return getInitialDirectories();
@@ -63,6 +88,11 @@ public class DirectoryManagement {
         }
     }
 
+    /**
+     * Lists the contents of a directory.
+     * @param directory the directory whose contents are to be listed
+     * @return an array of files/directories in the directory
+     */
     private File[] listDirectoryContent(File directory) {
         File[] contents = directory.listFiles();
         if (contents != null)
@@ -71,6 +101,10 @@ public class DirectoryManagement {
             return new File[0];
     }
 
+    /**
+     * Retrieves the initial set of directories based on the operating system.
+     * @return an array of files representing the root directories
+     */
     public File[] getInitialDirectories() {
         currentDirectory = "Root";
 
@@ -104,6 +138,11 @@ public class DirectoryManagement {
         return new File[0];
     }
 
+    /**
+     * Checks if the given directory represents the root.
+     * @param directory the directory to check
+     * @return true if the directory is the root, false otherwise
+     */
     private boolean isRootDirectory(String directory) {
         return directory == null || directory.equals("Root");
     }
