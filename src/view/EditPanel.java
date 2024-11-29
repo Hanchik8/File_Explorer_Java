903ff1e9513a3cd6874076415ebd87d31fcbb135
Baktiiar Barkhatbek uulu <barhatbekuulubaktiyar@gmail.com>
2024-11-30 02:17:45 +0600

    add detailsCheckBox and newComboBox

--- src/view/EditPanel.java
+++ src/view/EditPanel.java
@@ -3,6 +3,8 @@ package src.view;
 import java.awt.BorderLayout;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
 public class EditPanel {
@@ -14,12 +16,16 @@ public class EditPanel {
     private JButton pasteBtn;
     private JButton deleteBtn;
     private JButton renameBtn;
-    
+
+    private JCheckBox detailsCheckBox;
+
+    private JComboBox<String> newComboBox;
 
     public EditPanel() {
         editPanel = new JPanel(new BorderLayout());
 
         editPanel.add(createEditWestPart(), BorderLayout.WEST);
+        editPanel.add(createDetailsCheckBox(), BorderLayout.EAST);
     }
 
     /*
@@ -30,12 +36,14 @@ public class EditPanel {
     private JPanel createEditWestPart() {
         JPanel editWestPart = new JPanel();
 
+        editWestPart.add(createNewComboBox());
+
         editWestPart.add(createButton(cutBtn, "Cut"));
         editWestPart.add(createButton(copyBtn, "Copy"));
         editWestPart.add(createButton(pasteBtn, "Paste"));
         editWestPart.add(createButton(renameBtn, "Rename"));
         editWestPart.add(createButton(deleteBtn, "Delete"));
-        
+
         return editWestPart;
     }
 
@@ -47,30 +55,56 @@ public class EditPanel {
         return button;
     }
 
+    // creating detailsCheckBox
+    private JCheckBox createDetailsCheckBox() {
+        detailsCheckBox = new JCheckBox("Details");
+        detailsCheckBox.setSelected(true);
+
+        return detailsCheckBox;
+    }
+
+    // creating newComboBox
+    private JComboBox<String> createNewComboBox() {
+        String[] newListChoice = {"Create", "Directory", "Microsoft Word.docx", "Microsoft PowerPoint.pptx",
+        "Note.txt", "Microsoft Excel.xlsx"};
+        newComboBox = new JComboBox<>(newListChoice);
+        newComboBox.setSelectedItem("Create");
+
+        return newComboBox;
+    }
+
     /*
      * getters
      */
     public JPanel getEditPanel() {
         return editPanel;
     }
-    
+
     public JButton getCutBtn() {
         return cutBtn;
     }
-    
+
     public JButton getCopyBtn() {
         return copyBtn;
     }
-    
+
     public JButton getPasteBtn() {
         return pasteBtn;
     }
-    
+
     public JButton getDeleteBtn() {
         return deleteBtn;
     }
-    
+
     public JButton getRenameBtn() {
         return renameBtn;
+    }
+
+    public JCheckBox getDetailsCheckBox() {
+        return detailsCheckBox;
+    }
+
+    public JComboBox<String> getNewComboBox() {
+        return newComboBox;
     }
 }
