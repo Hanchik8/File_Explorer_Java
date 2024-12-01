3a4e012b32dc7b51cbf6ed2319ed222255853812
Alieva Nargiza <nargiza.alieva018@gmail.com>
2024-12-01 14:38:59 +0600

    create centerPanel view to show files

---
+++ src/view/CenterPanel.java
@@ -0,0 +1,48 @@
+package src.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Центральная панель для отображения списка файлов.
+ */
+public class CenterPanel {
+    private JPanel mainPanel; // Панель для всего содержимого
+    private DefaultListModel<String> fileListModel; // Модель данных для списка файлов
+    private JList<String> fileList; // Список файлов
+    public CenterPanel() {
+        mainPanel = new JPanel(new BorderLayout());
+        mainPanel.add(new JScrollPane(createFileList()), BorderLayout.CENTER);
+    }
+
+    /**
+     * Создаёт JList для отображения файлов.
+     * @return JList для отображения списка файлов.
+     */
+    private JList<String> createFileList() {
+        fileListModel = new DefaultListModel<>();
+        fileList = new JList<>(fileListModel);
+        return fileList;
+    }
+
+    /**
+     * Обновляет список файлов в модели с новыми данными.
+     * @param fileNamesList массив имен файлов для обновления.
+     */
+    public void updateFileListModel(String[] fileNamesList) {
+        fileListModel.clear();
+        for (String filename : fileNamesList) {
+            fileListModel.addElement(filename);
+        }
+    }
+
+    // ======== Геттеры для доступа к компонентам ========
+
+    public JList<String> getFileList() {
+        return fileList;
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
